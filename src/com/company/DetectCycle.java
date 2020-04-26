a40fd4f30630908c957033b3cbce3660bf165df2
vivek tiwari <vivek.t@zolostays.com>
2020-04-27 01:28:28 +0530

    Graphs

--- src/com/company/DetectCycle.java
+++ src/com/company/DetectCycle.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,6 +19,44 @@ public class DetectCycle {
 
         void addEdge(int v,int w){
             adj.get(v).add(w);
+        }
+
+        void addEdgeUndirected(int v,int w){
+            adj.get(v).add(w);
+            adj.get(w).add(v);
+        }
+        boolean hasCycleUndirected(int node, boolean[] visited, int parent){
+            visited[node] = true;
+
+            Iterator<Integer> iterator = this.adj.get(node).iterator();
+            while (iterator.hasNext()){
+                int value = iterator.next();
+
+                if (!visited[value]){
+                    if(hasCycleUndirected(value,visited,node)) return true;
+                } else {
+                    if (value != parent) return true;
+                }
+            }
+            return  false;
+        }
+
+        Boolean isCyclic()
+        {
+            // Mark all the vertices as not visited and not part of
+            // recursion stack
+            boolean visited[] = new boolean[V];
+            for (int i = 0; i < V; i++)
+                visited[i] = false;
+
+            // Call the recursive helper function to detect cycle in
+            // different DFS trees
+            for (int u = 0; u < V; u++)
+                if (!visited[u]) // Don't recur for u if already visited
+                    if (hasCycleUndirected(u, visited, -1))
+                        return true;
+
+            return false;
         }
 
     }
@@ -51,6 +90,7 @@ public class DetectCycle {
         }
     }
 
+
     public static void main(String[] a){
         Graph graph = new Graph(4);
         graph.addEdge(0, 1);
@@ -61,6 +101,8 @@ public class DetectCycle {
         graph.addEdge(3, 3);
 
         detectCycle(graph);
+
+        if (graph.isCyclic()) System.out.println("bla");
     }
 
 
